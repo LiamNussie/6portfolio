ade03ebd0e2fc03da2dfe54d5d34cdd4838b08f6
LiamNussie <65913823+LiamNussie@users.noreply.github.com>
2021-08-25 15:34:14 +0100

    90%

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/home/home';
+import Connect from './pages/connect/connect';
+import Invest from './pages/invest/invest';
+import Admin from './pages/admin/admin';
+import Distort from './pages/admin/distort/distort';
+import Mind from './pages/mind/mind';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer />
+        <Switch>
+           <Route exact path="/" component={Home} />
+           <Route exact path="/connect" component={Connect} />
+           <Route exact path="/invest" component={Invest} />
+           <Route exact path="/mind" component={Mind} />
+           <Route exact path="/broadcast-thoughts" component={Admin} />
+           <Route exact path="/broadcast-thoughts/edit" component={Distort} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
